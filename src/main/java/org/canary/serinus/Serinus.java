f62526aaa8639cbad95b19e756471eb34aa68c31
harveyk86 <harveyjk86+github@gmail.com>
2015-09-07 10:11:13 +0000

    Added basic Serinus application class

---
+++ src/main/java/org/canary/serinus/Serinus.java
@@ -0,0 +1,12 @@
+package org.canary.serinus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public final class Serinus {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Serinus.class, args);
+    }
+}

ea3e8ffbe8150ab8937d421149bf82b4058108c4
harveyk86 <harveyjk86+github@gmail.com>
2015-09-07 10:50:46 +0000

    Made Serinus class non-final for Spring

--- src/main/java/org/canary/serinus/Serinus.java
+++ src/main/java/org/canary/serinus/Serinus.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class Serinus {
+public class Serinus {
 
-    public static void main(final String[] args) {
+    public static final void main(final String[] args) {
         SpringApplication.run(Serinus.class, args);
     }
 }

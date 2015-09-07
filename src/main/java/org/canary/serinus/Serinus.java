2a9c1280f6c366d452ff5e1f8cd5fbed1823c46c
harveyk86 <harveyjk86+github@gmail.com>
2015-09-07 11:02:50 +0000

    Tidied up classes and added logging

--- src/main/java/org/canary/serinus/Serinus.java
+++ src/main/java/org/canary/serinus/Serinus.java
@@ -1,12 +1,25 @@
 package org.canary.serinus;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Serinus {
 
+    private static final Logger LOGGER = Logger.getLogger(Serinus.class);
+
+    protected Serinus() {
+
+        super();
+
+        LOGGER.trace("constructed");
+    }
+
     public static final void main(final String[] args) {
+
+        LOGGER.info("main[args=" + args + "]");
+
         SpringApplication.run(Serinus.class, args);
     }
 }
--- src/main/java/org/canary/serinus/controller/RootController.java
+++ src/main/java/org/canary/serinus/controller/RootController.java
@@ -1,13 +1,28 @@
 package org.canary.serinus.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public final class RootController {
 
+    private static final String ROOT_MESSAGE = "Hello, this is Serinus";
+
+    private static final Logger LOGGER = Logger.getLogger(RootController.class);
+
+    private RootController() {
+
+        super();
+
+        LOGGER.trace("constructed");
+    }
+
     @RequestMapping("/")
     public String root() {
-        return "Hello, this is Serinus!";
+
+        LOGGER.info("root");
+
+        return ROOT_MESSAGE;
     }
 }

8c4f9b0f32dd00efc6a9272918baf60684c39f7d
harveyk86 <harveyjk86+github@gmail.com>
2015-09-14 14:49:27 +0000

    Adjusted imports in Controller

--- src/main/java/org/canary/serinus/controller/Controller.java
+++ src/main/java/org/canary/serinus/controller/Controller.java
@@ -4,8 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.log4j.Logger;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.PathVariable;

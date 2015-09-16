5711d363306c8fa48cf5a3ee636b6a1f8b77c66d
harveyk86 <harveyjk86+github@gmail.com>
2015-09-16 14:05:41 +0000

    Added urlrewrite to Serinus class

--- src/main/java/org/canary/serinus/Serinus.java
+++ src/main/java/org/canary/serinus/Serinus.java
@@ -3,6 +3,9 @@ package org.canary.serinus;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
 
 @SpringBootApplication
 public class Serinus {
@@ -22,4 +25,26 @@ public class Serinus {
 
         SpringApplication.run(Serinus.class, args);
     }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+
+        LOGGER.info("filterRegistrationBean");
+
+        final FilterRegistrationBean filterRegistrationBean =
+            new FilterRegistrationBean();
+
+        final UrlRewriteFilter urlRewriteFilter = new UrlRewriteFilter();
+
+        filterRegistrationBean.setFilter(urlRewriteFilter);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("confPath", "urlrewrite.xml");
+
+
+        LOGGER.debug("filterRegistrationBean[returns=" + filterRegistrationBean
+            + "]");
+
+        return filterRegistrationBean;
+    }
+
 }
